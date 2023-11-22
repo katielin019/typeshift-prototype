39d088a06f1e47cb134e1b694de05e47af36cc11
Katie Lin <katielin019@gmail.com>
2023-11-21 22:43:19 -0500

    Trying to account for word length

--- Typeshift.py
+++ Typeshift.py
@@ -2,8 +2,8 @@
 # print("You entered: " + var_in)
 
 class Typeshift(object):
-    def __init__(self):
-        self.rows = []
+    def __init__(self, wordLength):
+        self.rows = [[]]
         self.index = 0
 
     def addColumn(letters):
@@ -11,6 +11,36 @@ class Typeshift(object):
         self.index += 1
 
 def game():
-    print("Welcome to the Typeshift solver.")
-    typeshiftInputted = false
-    col_0 = input("Input the first column of letters in your puzzle.")
+    wordLen = input('How long is each typeshift word?')
+    puzzle = Typeshift(int(wordLen))
+    initialized = False
+
+    while (not initialized):
+        x = input('Enter the letters (no spaces between) in column ' + str(puzzle.index) + ' or press enter/return to finish')
+        if len(x) == 0:
+            # https://stackoverflow.com/questions/52231554/blank-input-in-python
+            initialized = True
+        else:
+            # https://www.freecodecamp.org/news/python-string-to-array-how-to-convert-text-to-a-list/
+            puzzle.rows[puzzle.index] = list(x)
+            puzzle.index += 1
+
+    # call loadWords, which returns a list of words in wordlist that are the same length as puzzle.rows.length
+    words = loadWords(len(puzzle.rows))
+    print(len(words))
+
+def loadWords(target):
+    words = []
+    # https://stackoverflow.com/questions/70426424/convert-text-file-into-array
+    with open('wordlist-20210729.txt', 'r') as f:
+        for line in f:
+            word = line[1:-1].upper()
+            if len(word) == target:
+                words.append(word)
+    return words
+
+def main():
+    game()
+
+if __name__ == '__main__':
+    main()
