f1ef216027ba27ab0378f9015ce86b2b71a2884b
Katie Lin <katielin019@gmail.com>
2023-11-21 22:12:58 -0500

    Basic structure for game and class

--- Typeshift.py
+++ Typeshift.py
@@ -1,2 +1,16 @@
-var_in = input("Please enter something: ")
-print("You entered: " + var_in)
+# var_in = input("Please enter something: ")
+# print("You entered: " + var_in)
+
+class Typeshift(object):
+    def __init__(self):
+        self.rows = []
+        self.index = 0
+
+    def addColumn(letters):
+        self.rows[index] = letters
+        self.index += 1
+
+def game():
+    print("Welcome to the Typeshift solver.")
+    typeshiftInputted = false
+    col_0 = input("Input the first column of letters in your puzzle.")
