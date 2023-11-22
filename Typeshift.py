a0e2f3ff3a5172e8f62fff37fc6a48cc214e5c66
Katie Lin <katielin019@gmail.com>
2023-11-21 22:58:18 -0500

    For some reason, there's an additional quote character at the end of each word, but overall it basically works

--- Typeshift.py
+++ Typeshift.py
@@ -2,7 +2,7 @@
 # print("You entered: " + var_in)
 
 class Typeshift(object):
-    def __init__(self, wordLength):
+    def __init__(self):
         self.rows = [[]]
         self.index = 0
 
@@ -11,23 +11,35 @@ class Typeshift(object):
         self.index += 1
 
 def game():
-    wordLen = input('How long is each typeshift word?')
-    puzzle = Typeshift(int(wordLen))
+    # wordLen = input('How long is each typeshift word?')
+    puzzle = Typeshift()
     initialized = False
 
     while (not initialized):
-        x = input('Enter the letters (no spaces between) in column ' + str(puzzle.index) + ' or press enter/return to finish')
+        x = input('Enter the letters (no spaces between) in column ' + str(puzzle.index) + ' or press enter/return to finish: ')
         if len(x) == 0:
             # https://stackoverflow.com/questions/52231554/blank-input-in-python
             initialized = True
         else:
             # https://www.freecodecamp.org/news/python-string-to-array-how-to-convert-text-to-a-list/
             puzzle.rows[puzzle.index] = list(x)
+            puzzle.rows.append([])
             puzzle.index += 1
 
     # call loadWords, which returns a list of words in wordlist that are the same length as puzzle.rows.length
     words = loadWords(len(puzzle.rows))
-    print(len(words))
+    # print(len(words))
+    candidates = []
+    for word in words:
+        valid = True
+        for i in range(puzzle.index):
+            if word[i] not in puzzle.rows[i]:
+                valid = False
+        if valid == True:
+            candidates.append(word)
+    
+    print(candidates)
+    
 
 def loadWords(target):
     words = []
