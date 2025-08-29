501fed72c0294eb7e97dd0cfc99346a0fa039dfe
Katie Lin <katielin019@gmail.com>
2025-08-29 14:56:27 -0700

    add empty grid, add dummy button

---
+++ main.js
@@ -0,0 +1,5 @@
+// on keydown, make input toUpperCase
+// have buttons for expanding by a row / col
+
+// maybe hover over column = column color change
+// maybe at bottom, option to remove column (or above?)
